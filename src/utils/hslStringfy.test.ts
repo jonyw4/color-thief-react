da52063f6c2f04fd3fad0da09318bf69c0e9e9fd
Cage <cage@elnino.tech>
2021-07-07 13:15:00 +0200

    fix: update test values

--- src/utils/hslStringfy.test.ts
+++ src/utils/hslStringfy.test.ts
@@ -2,8 +2,8 @@ import hslStringfy from './hslStringfy';
 
 describe('hslStringfy', () => {
   test('should return a right string of HSL', () => {
-    const hslArray = [230, 102, 34];
-    const hsl = hslStringfy(hslArray[0], hslArray[1], hslArray[2]);
+    const hslArray = [21, 80, 52];
+    const hsl = hslStringfy(hslArray);
     expect(hsl).toBe('hsl(21, 80%, 52%)');
   });
 });
