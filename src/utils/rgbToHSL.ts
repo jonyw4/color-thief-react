e93b5919f4fed0114abb3662af0deea29cf0b182
Cage <cage@elnino.tech>
2021-07-07 13:19:06 +0200

    fix: update rgb to hsl algorithm

--- src/utils/rgbToHSL.ts
+++ src/utils/rgbToHSL.ts
@@ -2,28 +2,43 @@
  * Transform RGB to HSL
  */
 export default function rgbToHSL(r: number, g: number, b: number): number[] {
-  const _r = r / 255;
-  const _g = g / 255;
-  const _b = b / 255;
-  const max = Math.max(_r, _g, _b);
-  const min = Math.min(_r, _g, _b);
+  // Make r, g, and b fractions of 1
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  // Find greatest and smallest channel values
+  const cmin = Math.min(r, g, b);
+  const cmax = Math.max(r, g, b);
+  const delta = cmax - cmin;
   let h = 0;
   let s = 0;
-  const v = max;
-  if (max != min) {
-    const delta = max - min;
-    s = delta / max;
-    switch (max) {
-      case _r:
-        h = (_g - _b) / delta + (g < b ? 6 : 0);
-        break;
-      case _g:
-        h = (_b - _r) / delta + 2;
-        break;
-      case _b:
-        h = (_r - _g) / delta + 4;
-        break;
-    }
-  }
-  return [Math.round(h * 60), Math.round(s * 100), Math.round(v * 100)];
+  let l = 0;
+
+  // Calculate hue
+  // No difference
+  if (delta == 0) h = 0;
+  // Red is max
+  else if (cmax == r) h = ((g - b) / delta) % 6;
+  // Green is max
+  else if (cmax == g) h = (b - r) / delta + 2;
+  // Blue is max
+  else h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  // Make negative hues positive behind 360°
+  if (h < 0) h += 360;
+
+  // Calculate lightness
+  l = (cmax + cmin) / 2;
+
+  // Calculate saturation
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+  // Multiply l and s by 100
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return [Math.round(h), Math.round(s), Math.round(l)];
 }
