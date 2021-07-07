a9fd9495f6c9bd7a2ef730f883dfc822c80bbef6
Cage <cage@elnino.tech>
2021-07-07 13:15:26 +0200

    fix: update test match type

--- src/utils/rgbToHSL.test.ts
+++ src/utils/rgbToHSL.test.ts
@@ -4,6 +4,6 @@ describe('rgbToHSL', () => {
   test('should convert rgb to hsl correctly', () => {
     const rgbArray = [230, 102, 34];
     const hsl = rgbToHSL(rgbArray[0], rgbArray[1], rgbArray[2]);
-    expect(hsl).toBe([21, 80, 52]);
+    expect(hsl).toStrictEqual([21, 80, 52]);
   });
 });
