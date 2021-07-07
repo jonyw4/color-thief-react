79094484507e9402ef16ddb1b15677412d080ca6
Cage <cage@elnino.tech>
2021-07-07 12:45:00 +0200

    fix: lint colorFormats

--- src/types/ColorFormats.ts
+++ src/types/ColorFormats.ts
@@ -4,9 +4,14 @@
  * `hex` returns a color in hexadecimal format. `#000000`
  *
  * `rgbArray` returns a array of rgb. `[0, 0, 0]`
- * 
+ *
  * `hslString` returns an hsl string format. `hsl(0, 0, 0)`
- * 
+ *
  * `hslArray` returns a array of hsl. `[0, 0, 0]`
  */
-export type ColorFormats = 'rgbString' | 'hex' | 'rgbArray' | 'hslString' | 'hslArray';
+export type ColorFormats =
+  | 'rgbString'
+  | 'hex'
+  | 'rgbArray'
+  | 'hslString'
+  | 'hslArray';
