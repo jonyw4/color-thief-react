3ef845d18ab7a2844e8bff1a7be83abffd1b2eca
cagespence <42749119+cagespence@users.noreply.github.com>
2021-07-07 11:43:59 +0200

    Add HSL array and HSL string types to ColorFormats

--- src/types/ColorFormats.ts
+++ src/types/ColorFormats.ts
@@ -4,5 +4,9 @@
  * `hex` returns a color in hexadecimal format. `#000000`
  *
  * `rgbArray` returns a array of rgb. `[0, 0, 0]`
+ * 
+ * `hslString` returns an hsl string format. `hsl(0, 0, 0)`
+ * 
+ * `hslArray` returns a array of hsl. `[0, 0, 0]`
  */
-export type ColorFormats = 'rgbString' | 'hex' | 'rgbArray';
+export type ColorFormats = 'rgbString' | 'hex' | 'rgbArray' | 'hslString' | 'hslArray';
