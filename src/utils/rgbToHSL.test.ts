a79ab0403d11607631ce4a60055597709bcfc918
Cage <cage@elnino.tech>
2021-07-08 21:18:24 +0200

    feat: remove old color conversion files

--- src/utils/rgbToHSL.test.ts
+++
@@ -1,9 +0,0 @@
-import rgbToHSL from './rgbToHSL';
-
-describe('rgbToHSL', () => {
-  test('should convert rgb to hsl correctly', () => {
-    const rgbArray = [230, 102, 34];
-    const hsl = rgbToHSL(rgbArray[0], rgbArray[1], rgbArray[2]);
-    expect(hsl).toStrictEqual([21, 80, 52]);
-  });
-});
--- src/utils/rgbToHex.test.ts
+++
@@ -1,9 +0,0 @@
-import rgbToHex from './rgbToHex';
-
-describe('rgbToHex', () => {
-  test('should convert rgb to hex correctly', () => {
-    const rgbArray = [230, 102, 34];
-    const hex = rgbToHex(rgbArray[0], rgbArray[1], rgbArray[2]);
-    expect(hex).toBe('#e66622');
-  });
-});
