91a2047682af415b9f59a121601d58b8d1d6903c
Cage <cage@elnino.tech>
2021-07-11 18:14:17 +0200

    test: add tests for color conversion

--- src/utils/formatRGB.test.ts
+++ src/utils/formatRGB.test.ts
@@ -1,6 +1,7 @@
 import formatRGB from './formatRGB';
 
 const rgbArray: [number, number, number] = [230, 102, 34];
+const red: [number, number, number] = [255, 0, 0];
 const hslArray: [number, number, number] = [21, 80, 52];
 
 describe('formatRGB', () => {
@@ -18,5 +19,8 @@ describe('formatRGB', () => {
   });
   test('should return a string of hsl', () => {
     expect(formatRGB(rgbArray, 'hslString')).toBe('hsl(21, 80%, 52%)');
+  });
+  test('should return a css keyword', () => {
+    expect(formatRGB(red, 'keyword')).toBe('red');
   });
 });
