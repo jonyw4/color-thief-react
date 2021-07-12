b249553d9062cf6dd306c1d21fae2b2556a90940
Cage <cage@elnino.tech>
2021-07-12 19:29:34 +0200

    fix: update jsdoc

--- src/types/ColorFormats.ts
+++ src/types/ColorFormats.ts
@@ -8,6 +8,8 @@
  * `hslString` returns an hsl string format. `hsl(0, 0, 0)`
  *
  * `hslArray` returns a array of hsl. `[0, 0, 0]`
+ *
+ * `keyword` returns CSS color keyword (https://drafts.csswg.org/css-color/#named-colors)
  */
 export type ColorFormats =
   | 'rgbString'
